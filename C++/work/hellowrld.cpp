00919ad87c90e9a7a2449d1136275070e6d9c8b9
nick-san <hirohiro239@outlook.com>
2018-12-10 22:56:52 +0900

    はじめてのさくひん 自己満足のための解説は全部コメントで書いた

---
+++ C++/work/hellowrld.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+//Cでいうところのstdio.hライブラリ
+#include<string>
+//string型変数を扱う
+using namespace std;
+//stdという苗字のようなものを使う
+//（省略するために）
+
+int main(){
+  cout << "hello wrld" << endl;
+  //文字列はダブルコーテーションで囲う
+  //cout は画面を意味する
+  //改行は endl
+  //セミコロンをつける
+}
